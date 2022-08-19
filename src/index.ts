7c7918ff6ce18ca68d156f7bbbf263d821dcf5e0
Iran Adryan <contatoiranadryan@gmail.com>
2022-08-19 19:39:00 -0300

    feat: add cors to application

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import { routes } from './routes';
 import './database';
@@ -8,6 +9,7 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
