358d2f532bd9b501d6ba5c70a40b7ed14d185e7a
Iran Adryan <contatoiranadryan@gmail.com>
2022-08-24 10:38:03 -0300

    fix: change db config to online database

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,14 +2,14 @@ import { Options } from 'sequelize';
 
 export const dbConfig: Options = {
   dialect: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: 'root',
-  database: 'gsafra_mobile',
-  // host: 'br274.hostgator.com.br',
-  // username: 'coyot904_mobile',
-  // password: 'A@cessocli3nte8',
-  // database: 'coyot904_mobile',
+  // host: 'localhost',
+  // username: 'root',
+  // password: 'root',
+  // database: 'gsafra_mobile',
+  host: 'br274.hostgator.com.br',
+  username: 'coyot904_mobile',
+  password: 'A@cessocli3nte8',
+  database: 'coyot904_mobile',
   timezone: '-03:00',
   define: {
     timestamps: false,
